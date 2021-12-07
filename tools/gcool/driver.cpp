a51f5d8839fdab1768a64001bd1d2c078ba993db
Lin-Guanguo <lin.guanguo2000@gmail.com>
2021-12-07 16:23:00 +0800

    driver ing

--- tools/gcool/driver.cpp
+++ tools/gcool/driver.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
@@ -16,6 +17,32 @@ using namespace gcool;
 int main(int argc, char** argv) 
 {
     if (argc < 2) {
-        printf("gcool: Usage gcool file.cl");
+        printf("gcool: Usage gcool [file.cl]+\n");
+        return -1;
+    }
+
+
+    yyscan_t scanner;
+    yylex_init(&scanner);
+    for (int i = 1; i < argc; ++i) {
+        auto filename = argv[i];
+        printf("start compile %s\n", filename);
+        auto file = fopen(filename, "r");
+        if (file == NULL) {
+            printf("open file %s error\n", filename);
+            continue;
+        }
+        // lexer
+        yyrestart(file, scanner);
+
+        // parser
+        ast::ASTContext context;
+        Parser parser{scanner, &context};
+        parser.parse();
+
+        sema::Sema sema(&context);
+        sema.checkAll();
+        
+
     }
 }
