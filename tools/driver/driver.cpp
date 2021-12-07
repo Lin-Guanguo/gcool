7e5f9843c013ca72e09ade215f694474ba7ab98c
Lin-Guanguo <lin.guanguo2000@gmail.com>
2021-12-07 13:28:57 +0800

    add driver folder

---
+++ tools/driver/driver.cpp
@@ -0,0 +1,19 @@
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/STLExtras.h"
+#include "gcool/AST/Expr.h"
+#include "gcool/AST/AST.h"
+#include "gcool/Parser/Parser.h"
+#include "gcool/Lexer/Lexer.h"
+#include "gcool/Sema/Sema.h"
+#include "gcool/Basic/Diag.h"
+#include "gcool/PrettyPrinter/ASTPrinter.h"
+#include "gcool/LLVMIRGen/LLVMIRGen.h"
+
+using namespace gcool;
+
+int main(int argc, char** argv) 
+{
+    
+}
